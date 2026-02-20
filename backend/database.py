28c5379507d8f807ac5f92a61eb406e5d0bc2e3c
MonkyFlip <miguelhercerv@gmail.com>
2026-02-20 13:40:30 -0600

    atomizacion de variables de conexion

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/sge_db"
+load_dotenv()
+
+DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
