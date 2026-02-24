b522ea479960c374491b104bfd03a56eb1498509
MonkyFlip <miguelhercerv@gmail.com>
2026-02-23 22:14:30 -0600

    Reajuste en campos de servidor y cliente

--- backend/schemas/usuarios.py
+++ backend/schemas/usuarios.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 
 # Base compartido
@@ -9,39 +10,31 @@ class UsuarioBaseSchema(BaseModel):
     apellido_paterno: str
     apellido_materno: str
     email: str
-    password: str
-    email_verified: bool
-    email_verified_at: bool
-    created_at: str
-    updated_at: str
-    estado: str
 
 
 # Crear usuario
 class UsuarioCreateSchema(UsuarioBaseSchema):
-    rol_id: int
-    nombre: str
-    apellido_paterno: str
-    apellido_materno: str
-    email: str
     password: str
-    estado: str
 
 
-# Actualziar usuario
+# Actualizar usuario (parcial)
 class UsuarioUpdateSchema(BaseModel):
-    rol_id: int
-    nombre: str
-    apellido_paterno: str
-    apellido_materno: str
-    email: str
-    password: str
-    estado: str
+    rol_id: Optional[int] = None
+    nombre: Optional[str] = None
+    apellido_paterno: Optional[str] = None
+    apellido_materno: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
 
 
 # Respuesta
 class UsuarioResponseSchema(UsuarioBaseSchema):
     id_usuario: int
+    email_verified: bool
+    email_verified_at: Optional[datetime]
+    created_at: datetime
+    updated_at: datetime
+    estado: str
 
     class Config:
         from_attributes = True
